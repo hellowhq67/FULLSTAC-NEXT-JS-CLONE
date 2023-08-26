9eef88ed16d721a2333f29df493b92b5b32d31f6
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2023-08-26 22:43:39 +0600

    Update Home.jsx

--- componet/Home.jsx
+++ componet/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useState, useRef } from "react";
 import styles from "@/styles/Home.module.css";
-import { IoCopyOutline } from "react-icons/io";
-import { AiOutlineCheck } from "react-icons/ai";
+
 
 function Home() {
   const divRef = useRef(null);
@@ -37,7 +36,16 @@ function Home() {
           npx create-next-app@latest
         </div>
         <button className={styles.copybtn} onClick={handleCopyClick}>
-          {copied ? <AiOutlineCheck /> : "copy"}
+          {copied ?  <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-check2"
+              viewBox="0 0 16 16"
+            >
+              <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+            </svg>: "copy"}
         </button>
       </div>
     </div>
